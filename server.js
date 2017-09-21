b162d2b8d67e955de397719fed4269c941818adf
aragin <angiemywork2@yahoo.com>
2017-09-20 21:23:36 -0400

    server.js is new

--- server.js
+++ server.js
@@ -15,32 +15,81 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Hot Restaurant (DATA)
 // =============================================================
-var reservation = {
-  routeName: "reservation",
-  name: "",
-  phoneNumber: "",
-  email: "",
-  uniqueID: ""
-};
+var reservations = [{
+  routeName: "reserve",
+  name: "Angie",
+  phoneNumber: "919",
+  email: "week",
+  uniqueID: "Ang"
+}];
 
+// Routes
+app.get("/", function(req, res) {
+  res.send("Welcome to the Restaurant!");
+});
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "home.html"));
+// });
+
+app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
-});
-
 app.get("/all", function(req, res) {
-  res.sendFile(path.join(__dirname, "all.html"));
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-function makeReservation() {
-	for 
 
-}
+// Search for 5 reservations - provides JSON
+app.get("/api/reservations", function(req, res) {
+
+	res.json(reservations);
+//   var chosen = req.params.reservation;
+
+// // if (reservation.length < 6) {
+// // 	console.log(reservation.length);
+// // 	console.log("hi");
+
+
+//   if (chosen) {
+//     console.log(chosen);
+
+
+    // for (var i = 0; i < reservation.length; i++ ) {
+    //   if (chosen === reservation[i].routeName) {
+    //     return res.json(reservation[i]);
+    //   }
+    // }
+
+  })
+
+  app.post("/api/tables", function(req, res) {
+  var reservation = req.body;
+  // newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  // console.log(newcharacter);
+
+  reservations.push(reservation);
+
+  res.json(reservations);
+
+
+    
+  //}
+
+
+})
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
+});
+
+
 
 
 
